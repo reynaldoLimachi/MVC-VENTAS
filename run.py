8ccafee27719350405318e9d878f5053a9567897
reynaldo <reylimachi09@gmail.com>
2025-11-18 14:10:51 -0400

    tercer comit

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 from controllers import usuario_controller
 from controllers import cliente_controller
@@ -26,7 +26,7 @@ def inject_active_path():
 
 @app.route('/')
 def home():
-    return '<h1>Aplicacion ventas</h1>'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     with app.app_context():
